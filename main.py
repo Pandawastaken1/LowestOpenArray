1642185c0868c17de0a0c43f9ed1a15003a37ba9
jeff bezos <aaakashgamer45@outlook.com>
2020-08-28 23:26:57 +0000

    ill eat ur lunch

--- main.py
+++ main.py
@@ -11,11 +11,7 @@ class bus:
     print("and this is just like a movie")
 
 
-    #0 100,22,44,33
-    #2.5,
-    #"Hello George, how are you today? Just Fine?"
-    #True/False
-
+    
 
 mybus=bus("black",40,3050.1,True)
 
